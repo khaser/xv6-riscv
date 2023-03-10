6bbd57adfc49ef9beb5c8ce878af95a2067a5d2d
khaser <a-horohorin@mail.ru>
2023-03-10 13:35:50 +0300

    postreview fix

--- user/task-1-msg.c
+++ user/task-1-msg.c
@@ -6,55 +6,57 @@ const int MAXN = 255;
 const char* fmt = "%d: received %c\n";
 
 int main(int argc, const char* argv[]){
-    if (argc < 2) {
-        printf("Expected at least 1 argument\n");
-        exit(1);
+  if (argc < 2) {
+    printf("Expected at least 1 argument\n");
+    exit(1);
+  }
+  const char* msg = argv[1];
+  close(0);
+
+  int parent2child[2];
+  int child2parent[2];
+  if (pipe(parent2child) < 0 || pipe(child2parent) < 0) {
+    printf("Creating pipe error\n");
+    exit(1);
+  }
+
+  int child = fork();
+  int pid = getpid();
+  if (child < 0) {
+    printf("Fork error\n");
+    exit(1);
+  }
+  if (child == 0) {
+    // child
+    close(parent2child[1]);
+    close(child2parent[0]);
+
+    char c;
+    while (read(parent2child[0], &c, 1) == 1) {
+      printf(fmt, pid, c);
+      write(child2parent[1], &c, 1);
     }
-    const char* msg = argv[1];
-    close(0);
+    close(1);
+    close(child2parent[1]);
+    close(parent2child[0]);
+  } else {
+    // parent
+    close(parent2child[0]);
+    close(child2parent[1]);
 
-    int parent2child[2];
-    int child2parent[2];
-    pipe(parent2child);
-    pipe(child2parent);
-
-    int child = fork();
-    int pid = getpid();
-    if (child < 0) {
-        printf("Fork error\n");
-        exit(1);
+    for (int i = 0; msg[i]; ++i) {
+      write(parent2child[1], &msg[i], 1);
     }
-    if (child == 0) {
-        // child
-        close(parent2child[1]);
-        close(child2parent[0]);
+    close(parent2child[1]);
 
-        char c;
-        while (read(parent2child[0], &c, 1) == 1) {
-            printf(fmt, pid, c);
-            write(child2parent[1], &c, 1);
-        }
-        close(1);
-        close(child2parent[1]);
-        close(parent2child[0]);
-    } else {
-        // parent
-        close(parent2child[0]);
-        close(child2parent[1]);
-
-        for (int i = 0; i < strlen(msg); ++i) {
-            write(parent2child[1], &msg[i], 1);
-        }
-        close(parent2child[1]);
-
-        wait(0);
-        char c;
-        while (read(child2parent[0], &c, 1) == 1) {
-            printf(fmt, pid, c);
-        }
-        close(1);
-        close(child2parent[0]);
-        close(parent2child[1]);
+    wait(0);
+    char c;
+    while (read(child2parent[0], &c, 1) == 1) {
+      printf(fmt, pid, c);
     }
-    exit(0);
+    close(1);
+    close(child2parent[0]);
+    close(parent2child[1]);
+  }
+  exit(0);
 }
--- user/task-1-wc.c
+++ user/task-1-wc.c
@@ -5,35 +5,38 @@
 const int MAXN = 255;
 
 int main() {
-    int ends[2];
-    pipe(ends);
+  int ends[2];
+  if (pipe(ends) < 0)  {
+    printf("Creating pipe error\n");
+    exit(1);
+  }
 
-    int child = fork();
-    if (child < 0) {
-        printf("Fork error\n");
-        exit(1);
-    }
-    if (child == 0) {
-        // child
-        close(0);
-        dup(ends[0]);
+  int child = fork();
+  if (child < 0) {
+    printf("Fork error\n");
+    exit(1);
+  }
+  if (child == 0) {
+    // child
+    close(0);
+    dup(ends[0]);
 
-        close(ends[1]);
+    close(ends[1]);
 
-        char* argv[] = {"wc", 0};
-        exec("/wc", argv);
-    } else {
-        // parent
-        close(ends[0]);
-        close(1);
+    char* argv[] = {"wc", 0};
+    exec("/wc", argv);
+  } else {
+    // parent
+    close(ends[0]);
+    close(1);
 
-        char buff[MAXN];
-        int len = read(0, buff, 256);
-        close(0);
+    char buff[MAXN];
+    int len = read(0, buff, MAXN);
+    close(0);
 
-        write(ends[1], buff, len);
-        close(ends[1]);
-        wait(0); // for pretty formating
-    }
-    exit(0);
+    write(ends[1], buff, len);
+    close(ends[1]);
+    wait(0); // for pretty formating
+  }
+  exit(0);
 }
